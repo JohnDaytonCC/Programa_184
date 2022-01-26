a47a02b3e843e54f9982392320685a34ee379984
JohnDaytonCC <john.dayton.cc@gmail.com>
2022-01-26 13:53:10 -0300

    Implementação mais adequada para abrir arquivos

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,12 +9,8 @@ public class Program {
 	public static void main(String[] args) {
 
 		String path = "E:\\JOHN DAYTON\\CURSOS DE EXTENSÃO\\Java Completo\\Comandos_do_GIT.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
 		
-		try{
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
+		try(BufferedReader br = new BufferedReader(new FileReader(path))){
 			
 			String line = br.readLine();
 			
@@ -25,15 +21,6 @@ public class Program {
 		}
 		catch(IOException e) {
 			System.out.println("Error: " + e.getMessage());
-		}
-		finally {
-			try {
-				if(br != null) br.close();
-				if(fr != null) fr.close();
-			}
-			catch(IOException e){
-				e.printStackTrace();
-			}
 		}
 	}
 
